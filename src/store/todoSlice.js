8f6e5e67bb3d2ef19b56dd1c211db07aa9d0454b
KoldaevAlexey <koldaev.alexey@gmail.com>
2023-06-05 07:29:42 +0300

    .

--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -1,8 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchTodos = createAsyncThunk(
+    "todos/fetchTodos",
+    async function (_, { rejectWithValue }) {
+        try {
+            const response = await fetch(
+                "https://jsonplaceholder.typicode.com/todos?_limit=10"
+            );
+
+            if (!response.ok) {
+                throw new Error("can't fetch");
+            }
+
+            const data = await response.json();
+
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+const setError = (state, action) => {
+    state.status = "rejected";
+    state.error = action.error.message;
+};
 
 const todoSlice = createSlice({
     name: "todos",
-    initialState: [],
+    initialState: {
+        todos: [],
+        status: null,
+        error: null,
+    },
     reducers: {
         addTodo(state, action) {
             state.push({
@@ -20,6 +50,17 @@ const todoSlice = createSlice({
         removeTodo(state, action) {
             return state.filter((todo) => todo.id !== action.payload);
         },
+    },
+    extraReducers: {
+        [fetchTodos.pending]: (state) => {
+            state.status = "loading";
+            state.error = null;
+        },
+        [fetchTodos.fulfilled]: (state, action) => {
+            state.status = "resolved";
+            state.todos = action.payload;
+        },
+        [fetchTodos.rejected]: setError,
     },
 });
 
